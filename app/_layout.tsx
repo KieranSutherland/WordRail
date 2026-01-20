2dde310b284cce8b4425ebd3efa5f2c86761a341
Kieran Sutherland <35941396+KieranSutherland@users.noreply.github.com>
2026-01-20 23:58:50 +0000

    Add vercel metrics and attempt to fix icon issue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,31 @@
 import { Stack } from 'expo-router';
-
+import * as Font from "expo-font";
 import { useAppTheme } from '../components/ThemeProvider';
 import '../global.css';
 import ThemeProvider from '../components/ThemeProvider';
+import { useEffect, useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 export default function RootLayout() {
     const { colors } = useAppTheme();
-    
+    const [ fontsLoaded, setFontsLoaded ] = useState(false);
+
+    useEffect(() => {
+        Font.loadAsync(Ionicons.font).then(() => {
+            setFontsLoaded(true);
+        });
+    }, []);
+
+    if (!fontsLoaded) {
+        return null; // or a splash/loading screen
+    }
+
     return (
         <ThemeProvider>
+            <Analytics />
+            <SpeedInsights />
             <Stack
                 screenOptions={ {
                     headerShown: false,
